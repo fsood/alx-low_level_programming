bead4627df8ffb3ffa31bc7c71f7ef7787bae9fd
fsood <75765092+fsood@users.noreply.github.com>
2023-02-21 21:01:50 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,24 @@
 #include "main.h"
+
 /**
- * print_alphabet_x10 -  prints 10 times the alphabet,
- * in lowercase, followed by a new line.
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return:void
  */
+
 void print_alphabet_x10(void)
 {
-int x;
-int count;
-for (count = 0; count <= 9; count++)
-{
-for (x = 'a'; x <= 'z'; x++)
-{
-_putchar(x);
-}
-_putchar('\n');
-}
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
