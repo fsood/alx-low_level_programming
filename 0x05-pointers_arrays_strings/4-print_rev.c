473c0d86f56b3b1c845f50f7f5fe2b9791847511
fsood <75765092+fsood@users.noreply.github.com>
2023-03-04 15:59:17 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,24 @@
+#include "main.h"
+/**
+ *print_rev - prints strin in reverse.
+ *@s: pointer to string.
+ *
+ *Return: void.
+ */
+void print_rev(char *s)
+{
+	int length, last;
 
-  #include "main.h"
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
 
-  /** 
-   * print_rev- print reversed string, followed by a new line
-   * @s: pointer to the string to point 
-   * Return: void
-   */
-
-   void _print_rev(char *s) 
-  {
-     int swap;
-
-     int i = 0;
-     while (s[i]) 
-            i++;
-  while (i--) 
-{  
-      _putchar(s[i]);
-  } 
-     _putchar('/n');
-
-  }
+	last = length - 1;
+	for (; last >= 0; last--)
+	{
+		_putchar(s[last]);
+	}
+	_putchar('\n');
+}
